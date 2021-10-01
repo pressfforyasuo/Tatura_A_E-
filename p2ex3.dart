8e95702a66567bcb3cac658675a400f5002bf7d8
pressfforyasuo <m2003573@edu.misis.ru>
2021-10-01 22:29:24 +0300

    p 2 ex 3

---
+++ p2ex3.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main(){
+
+  List<int> input = [];
+  input.add(-999);
+
+  stdin.readLineSync()!.split(' ').forEach((element) {
+    input .add(int.parse(element));
+  });
+
+  var sum=0;
+
+  for(var i = 0; i<input.length;i++){
+    if(input[i]%2==0){
+      sum+=input[i];
+    }
+  }
+
+  sum==0? print("NO") : print(sum);
+
+}
